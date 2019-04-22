e8d08f7c22dd277c6b79e01aeca5334991916dad
Dong <690443961@qq.com>
2019-04-22 09:17:20 +0800

    图片自适应

---
+++ utils/imageUtil.js
@@ -0,0 +1,38 @@
+//util.js
+function imageUtil(height, width) {
+  var imageSize = {};
+  var originalWidth = width;//图片原始宽
+  var originalHeight = height;//图片原始高
+  //图片高宽比
+  var originalScale = originalHeight / originalWidth 
+  console.log('originalWidth: ' + originalWidth)
+  console.log('originalHeight: ' + originalHeight)
+  //获取屏幕宽高
+  wx.getSystemInfo({
+    success: function (res) {
+      var windowWidth = res.windowWidth;
+      var windowHeight = res.windowHeight;
+      //屏幕高宽比
+      var windowscale = windowHeight / windowWidth;
+      console.log('windowWidth: ' + windowWidth)
+      console.log('windowHeight: ' + windowHeight)
+      if (originalScale < windowscale) {//图片高宽比小于屏幕高宽比
+        //图片缩放后的宽为屏幕宽
+        imageSize.imageWidth = windowWidth;
+        imageSize.imageHeight = (windowWidth * originalHeight) / originalWidth;
+      } else {//图片高宽比大于屏幕高宽比
+        //图片缩放后的高为屏幕高
+        imageSize.imageHeight = windowHeight;
+        imageSize.imageWidth = (windowHeight * originalWidth) / originalHeight;
+      }
+
+    }
+  })
+  console.log('缩放后的宽: ' + imageSize.imageWidth)
+  console.log('缩放后的高: ' + imageSize.imageHeight)
+  return imageSize;
+}
+
+module.exports = {
+  imageUtil: imageUtil
+}
